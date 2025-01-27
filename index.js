43fb6436563c97fffbfce03becc381eafbda1270
Patrick Neicu <patrickneicu2006@gmail.com>
2025-01-28 00:15:40 +0200

    sorry

--- index.js
+++ index.js
@@ -50,34 +50,31 @@ app.delete('/api/notes/:id' , (req, res) => {
 })
 
 const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => Number(n.id))) 
-      : 0
-
-    return String(maxId + 1)
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
 }
+
 app.post('/api/notes', (request, response) => {
-  
-    const note = request.body
+  const body = request.body
 
-    if (!body.content) {
-        return response.status(400).json({ 
-          error: 'content missing' 
-        })
-      }
-    
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
 
-    note ={
-        'content': body.content , 
-        'important': Boolean(body.important) || false , 
-        'id':generateId()
-    }
-  
-    notes = notes.concat(note)
-  
-    response.json(note)
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  }
 
-  })
+  notes = notes.concat(note)
+
+  response.json(note)
+})
 
 
 const PORT = process.env.PORT || 3001
