2905bdf926c8a10ef1c19b9a66f2e620b9486a14
Patrick Neicu <patrickneicu2006@gmail.com>
2025-01-30 14:56:00 +0200

    add env

--- index.js
+++ index.js
@@ -6,41 +6,23 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+const Note = require('models/Note')
 
 app.get('/' , (req , res) => {
     res.send("<div> <h1> Hello World </h1> </div>")
 })
 
 app.get('/api/notes' , (req,res) => {
-    res.json(notes)
+    Note.find({}).then(result => {
+      res.json(result)
+    })
 })
 
 app.get('/api/notes/:id' , (req,res) => {
-    id=req.params.id
-    note=notes.find(note => note.id==id)
-    if(note){
-        res.json(note)
-    }else{
-        res.status(404)
-        res.send("<h1>404</h1>")
-    }
+    const id =req.params.id
+    Note.findById(id).then((note) =>{
+      res.json(note)
+    })
 })
 
 app.delete('/api/notes/:id' , (req, res) => {
